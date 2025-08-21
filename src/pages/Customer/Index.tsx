a87c7cb178eba55d7dbe0a3e8ecd2479fe61ea31
heangdeveloper <simkimheang4@gmail.com>
2025-08-21 16:45:58 +0700

    update file

--- src/pages/Customer/Index.tsx
+++ src/pages/Customer/Index.tsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
+import Paper from '@mui/material/Paper'
+import TableContainer from '@mui/material/TableContainer';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 
 const ListCustomers = () => {
     return (
         <>
-            List Customers
+            <Stack></Stack>
+            <Paper elevation={0}>
+                <TableContainer>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "space-between",
+                            padding: "24px",
+                        }}
+                    >
+                        <Box
+                            sx={{
+                                marginLeft: "auto",
+                            }}
+                        >
+                            <FormControl>
+                                <TextField size="small" sx={{ width: "200px" }} placeholder="Search..."/>
+                            </FormControl>
+                        </Box>
+                    </Box>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "space-between",
+                            padding: "24px",
+                        }}
+                    >
+                        <Box>
+                            <Typography variant="button">Showing 1 to 10 of 12 entries</Typography>
+                        </Box>
+                        <Box>
+                            
+                        </Box>
+                    </Box>
+                </TableContainer>
+            </Paper>
         </>
     )
 }
--- src/themes/customizations/inputs.tsx
+++ src/themes/customizations/inputs.tsx
@@ -1,9 +1,42 @@
 import type { Theme, Components, alpha } from '@mui/material'
+import { outlinedInputClasses } from '@mui/material/OutlinedInput';
 
 export const inputsCustomizations: Components<Theme> = {
     MuiInputBase: {
         styleOverrides: {
             root: {
+                border: "none"
+            }
+        }
+    },
+    MuiOutlinedInput: {
+        styleOverrides: {
+            input: {
+                padding: "0",
+            },
+            root: ({ theme }) => ({
+                padding: "8px 12px",
+                fontSize: "12px",
+                color: (theme.vars || theme).palette.text.primary,
+                borderRadius: (theme.vars || theme).shape.borderRadius,
+                outline: `1px solid ${(theme.vars || theme).palette.divider}`,
+                backgroundColor: (theme.vars || theme).palette.background.default,
+                transition: "outline 120ms ease-in",
+                "&:hover": {
+                    outline: "1px solid",
+                },
+                variants: [
+                    {
+                        props: {
+                            size: "small",
+                        },
+                        style: {
+                            height: "34px"
+                        }
+                    },
+                ]
+            }),
+            notchedOutline: {
                 border: "none"
             }
         }
